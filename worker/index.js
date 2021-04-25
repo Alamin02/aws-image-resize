facfb8f1af75ac4183ca4ef4d895bbd0c26a5e9c
Md. Al Amin <alamin.ece.ku@gmail.com>
2021-04-25 18:42:24 +0600

    Worker image processing subroutine

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,20 @@
+import Jimp from "jimp";
+import path from "path";
+
 const AWS = require("aws-sdk");
+
+const samplePath = path.join(__dirname, "sample", "sample.jpg");
+
+async function processImage(width, height) {
+  const image = await Jimp.read(samplePath)
+    .then((image) => {
+      return image.resize(width, height).getBufferAsync(image.getMIME());
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  console.log(image);
+}
+
+process(320, 200);
